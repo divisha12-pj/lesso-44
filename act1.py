c02d1bbc43dad0f1bcf8462e9e51b10e86523f4d
divishajain12-pj <divishajain1210@gmail.com>
2024-11-22 21:31:48 +0530

    act1

---
+++ act1.py
@@ -0,0 +1,19 @@
+tup1=(1,2,3,4,5)
+print("tup1 is",tup1)
+
+
+tup2=("cheeseecake",[1,2,4],[8,9,7])
+print(tup2 [0])
+print(tup2  [1][1])
+print(tup2 [2][2])
+
+
+tup3=('c','o','d','i','n','g')
+for i in tup3:
+    print("hello",i)
+
+tup4=(9,)
+tup5=tup1+tup4
+print("tup5 is",tup5)
+
+print("slice tup is",tup5[1:4])
